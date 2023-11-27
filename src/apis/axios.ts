5fc22ee87992fc79b4c0bfeacd14009eab5050e2
eternrust <overload829@gmail.com>
2023-11-27 20:06:36 +0900

    ⚙️ :: axios 설정

---
+++ src/apis/axios.ts
@@ -0,0 +1,27 @@
+import axios, { AxiosError } from 'axios';
+
+export const instance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  timeout: 10000,
+});
+
+instance.interceptors.request.use(
+  async function (config) {
+    const accessToken = localStorage.getItem("access_token");
+    if (accessToken) {
+      //@ts-ignore
+      config.headers = {
+        Authorization: `Bearer ${accessToken}`,
+        "X-Not-Using-Xquare-Auth": "true",
+      };
+    }
+    return config;
+  },
+  function (error: AxiosError) {
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(undefined, function (error) {
+  throw error;
+});
