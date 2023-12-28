d66ac7ab3d22bbacc3ce2c040d2560e3ab03be25
eternrust <eternrust@dsm.hs.kr>
2023-12-28 11:06:27 +0900

    ⚓️ :: toast hover시 toast 사라지는 시간 정지

--- src/utils/store/modules/toast.ts
+++ src/utils/store/modules/toast.ts
@@ -4,50 +4,39 @@ import type { PayloadAction } from "@reduxjs/toolkit";
 
 export type ToastmessageType = 'success' | 'error' | 'loading';
 
-interface createToastType {
+interface ToastStateType {
   id: string;
   message: string;
   type: ToastmessageType;
   duration: number;
 }
 
-interface ToastStateType extends createToastType {
-  isVisible: boolean;
-}
-
 const initialState: ToastStateType[] = [];
 
-export const showToastAsync = createAsyncThunk<void, createToastType>(
+export const showToastAsync = createAsyncThunk<void, ToastStateType>(
   "toast/showToastAsync",
   async (payload, { dispatch, getState }) => {
-    const { id, duration } = payload;
+    const { id } = payload;
     const { toast: toastData } = getState() as { toast: ToastStateType[] };
     const key = toastData.map((value) => value.id);
-    
-    if(key.includes(id)) {
-      dispatch(patchToast({...payload, isVisible: true }));
+
+    if (key.includes(id)) {
+      dispatch(patchToast(payload));
     } else {
       dispatch(createToast(payload));
     }
-    
-    await new Promise((resolve) => setTimeout(resolve, duration));
-    // dispatch(patchToast({...payload, isVisible: false }));
-    
-    // await new Promise((resolve) => setTimeout(resolve, 1000));
-    dispatch(deleteToast({ id }));
   })
 
 const toastReducer = createSlice({
   name: "toast",
   initialState,
   reducers: {
-    createToast: (state, action: PayloadAction<createToastType>) => {
+    createToast: (state, action: PayloadAction<ToastStateType>) => {
       state.push({
         id: action.payload.id,
         message: action.payload.message,
         type: action.payload.type,
-        duration: action.payload.duration,
-        isVisible: true
+        duration: action.payload.duration
       });
     },
     deleteToast: (state, action: PayloadAction<{ id: string }>) => {
