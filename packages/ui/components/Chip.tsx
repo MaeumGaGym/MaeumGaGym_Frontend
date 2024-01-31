029d31f8f2f83aa3b7ed1bbfe9b8b98a0bdaeef4
eternrust <eternrust@dsm.hs.kr>
2024-01-31 23:36:11 +0900

    💉 :: ui package webpack 삭제
    'use client'의 위치 옮김

--- packages/ui/components/Chip.tsx
+++ packages/ui/components/Chip.tsx
@@ -1,4 +1,3 @@
-'use client'
 import { MouseEvent, ReactNode } from 'react'
 
 interface ChipProps {
--- packages/ui/components/Header.tsx
+++ packages/ui/components/Header.tsx
@@ -1,4 +1,3 @@
-'use client'
 import Image from 'next/image'
 import { Button } from './Button'
 import { HeaderLogo } from '../assets'
