44a0ef87a3926870812f15d8853044e1e4035144
eternrust <eternrust@dsm.hs.kr>
2024-02-04 21:55:45 +0900

    ⚙️ :: CI/CD 테스트를 위한 commit

--- service/main/src/app/page.tsx
+++ service/main/src/app/page.tsx
@@ -4,7 +4,7 @@ import { Footer } from '@package/ui'
 const Home = () => {
   return (
     <div>
-      <div className="text-gray400">아마도? 메인페이지</div>
+      <div className="text-gray500">아마도? 메인페이지</div>
       <Footer />
     </div>
   )
