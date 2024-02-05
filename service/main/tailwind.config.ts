ef248af886f9fdab304b074d4102d6b91e6b680c
SeungWoo <wjknn3123@gmail.com>
2024-02-05 22:57:44 +0900

    ✏ :: edit tailwind font weight

--- service/main/tailwind.config.ts
+++ service/main/tailwind.config.ts
@@ -6,8 +6,8 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/assets/**/*.{js,ts,jsx,tsx,mdx}',
     './src/utils/**/*.{js,ts,jsx,tsx,mdx}',
-    "../../packages/ui/components/**/*.{js,ts,jsx,tsx}",
-    "../../packages/ui/assets/**/*.{js,ts,jsx,tsx}",
+    '../../packages/ui/components/**/*.{js,ts,jsx,tsx}',
+    '../../packages/ui/assets/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: 'class',
   theme: {
@@ -79,35 +79,35 @@ module.exports = {
       headlineMedium: [
         '48px',
         {
-          fontWeight: 600,
+          fontWeight: 700,
           lineHeight: '64px',
         },
       ],
       headlineSmall: [
         '40px',
         {
-          fontWeight: 500,
+          fontWeight: 600,
           lineHeight: '52px',
         },
       ],
       titleLarge: [
         '36px',
         {
-          fontWeight: 500,
+          fontWeight: 600,
           lineHeight: '48px',
         },
       ],
       titleMedium: [
         '24px',
         {
-          fontWeight: 500,
+          fontWeight: 600,
           lineHeight: '32px',
         },
       ],
       titleSmall: [
         '16px',
         {
-          fontWeight: 500,
+          fontWeight: 600,
           lineHeight: '24px',
         },
       ],
