ac310435466a23cf481b56b7741775ba5b124f4f
SeungWoo <wjknn3123@gmail.com>
2024-02-16 01:44:16 +0900

    ⚙️ :: proxy setting

--- service/main/next.config.js
+++ service/main/next.config.js
@@ -3,10 +3,19 @@
 
 const nextConfig = {
   swcMinify: true,
+  reactStrictMode: false,
   // output: 'standalone',
   // experimental: {
   //   outputFileTracingRoot: path.join(__dirname, '../../'),
   // }
-};
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://stag-server.xquare.app/:path*',
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
