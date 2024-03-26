decde57578f9bc099dc08a3b4675739c79837053
ojinikim <ojinikim@gmail.com>
2024-03-26 12:21:48 +0900

    ADD :: API connection settings

---
+++ service/user/src/apis/pose/getPoses.ts
@@ -0,0 +1,18 @@
+import { getCookie } from './../../../../main/src/utils/cookie'
+import { instance } from '../axios'
+
+export const getPoses = async (access_token: string) => {
+  return await instance({
+    method: 'GET',
+    url: `/poses`,
+    headers: {
+      // 'X-XSRF-TOKEN': rf_token,
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${access_token}`,
+    },
+  })
+    .then(res => res.data)
+    .catch(err => {
+      console.log('이런', err)
+    })
+}
