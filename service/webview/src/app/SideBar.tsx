f603038555d7143025d9992bce3c63414bb3c707
wlals <jiminelp@gmail.com>
2024-04-10 09:17:53 +0900

    FEAT :: 사이드바 기능 추가
    Like 클릭, 댓글 클릭, 공유 클릭

---
+++ service/webview/src/app/SideBar.tsx
@@ -0,0 +1,39 @@
+import { Dots, Heart, Message, Share } from '@package/ui'
+import { useState } from 'react'
+
+interface propsType {
+  setCommentOpen: () => void
+  like: boolean
+  setLike: () => void
+  setShareOpen: () => void
+}
+
+const SideBar = ({ setCommentOpen, like, setLike, setShareOpen }: propsType) => {
+  return (
+    <div className="w-[80px] h-fit py-[40px] gap-[24px] flex flex-col absolute bottom-0 right-0 items-center z-40">
+      <div className="flex flex-col items-center cursor-pointer" onClick={setLike}>
+        <div className="flex justify-center items-center w-[48px] h-[48px]">
+          <Heart isFill className={`${like ? 'text-red600' : 'text-white'}`} size={32} />
+        </div>
+        <span className={`text-white text-bodySmall`}>36.1만</span>
+      </div>
+      <div className="flex flex-col items-center cursor-pointer" onClick={setCommentOpen}>
+        <div className="flex justify-center items-center w-[48px] h-[48px]">
+          <Message className="text-white" size={32} />
+        </div>
+        <span className="text-bodySmall text-white">339</span>
+      </div>
+      <div className="flex flex-col items-center cursor-pointer" onClick={setShareOpen}>
+        <div className="flex justify-center items-center w-[48px] h-[48px]">
+          <Share className="text-white" size={32} />
+        </div>
+        <span className="text-bodySmall text-white">공유</span>
+      </div>
+      <div className="flex justify-center items-center w-[48px] h-[48px] cursor-pointer">
+        <Dots className="text-white" size={32} />
+      </div>
+    </div>
+  )
+}
+
+export default SideBar
---
+++ service/webview/src/app/VideoPlayer.tsx
@@ -0,0 +1,99 @@
+'use client'
+
+import { Heart, Pause, Play, Share } from '@package/ui'
+import Hls from 'hls.js'
+import { useEffect, useRef, useState } from 'react'
+import Content from './Content'
+import SideBar from './SideBar'
+import Comments from './Comment'
+import ShareContainer from './Share'
+
+const VideoPlayer = ({ src, videoId }: { src: string; videoId: string }) => {
+  const [pause, setPause] = useState<boolean>(false)
+  const [commentOpen, setCommentOpen] = useState<boolean>(false)
+  const [shareOpen, setShareOpen] = useState<boolean>(false)
+  const [like, setLike] = useState<boolean>(false)
+  const videoRef = useRef<HTMLVideoElement>(null)
+
+  const onPause = () => {
+    if (videoRef.current) {
+      !pause ? videoRef.current.pause() : videoRef.current.play()
+      setPause(v => !v)
+    }
+  }
+
+  const handleCommentOpen = () => {
+    setCommentOpen(!commentOpen)
+  }
+
+  const handleLike = () => {
+    setLike(!like)
+  }
+
+  const handleShareOpen = () => {
+    setShareOpen(!shareOpen)
+  }
+
+  useEffect(() => {
+    const handleCanPlayThrough = () => {
+      if (videoRef.current) {
+        videoRef.current.play()
+        videoRef.current.muted = true
+        setPause(false)
+        videoRef.current.removeEventListener('canplaythrough', handleCanPlayThrough)
+      }
+    }
+
+    if (Hls.isSupported() && videoRef.current) {
+      const hls = new Hls()
+      hls.loadSource(src)
+      hls.attachMedia(videoRef.current)
+      videoRef.current.muted = true
+      videoRef.current.addEventListener('canplaythrough', handleCanPlayThrough)
+    }
+
+    return () => {
+      videoRef.current?.removeEventListener('canplaythrough', handleCanPlayThrough)
+    }
+  }, [src])
+
+  return (
+    <div
+      className={`bg-cover rounded-[8px] lg:aspect-[9/16] md:aspect-[9/16] w-full sm:h-full relative`}
+      style={{ backgroundImage: `url('https://storage.pokabook.kr/maeumgagym/${videoId}/thumbnail.png')` }}
+    >
+      <div className="loader" />
+      <video
+        ref={videoRef}
+        className="w-full h-full object-cover lg:rounded-[8px] md:rounded-[8px] z-10"
+        loop
+        onClick={onPause}
+      />
+      {like ? (
+        <div className="absolute w-[80px] h-[80px] bg-[rgba(0,0,0,0.4)] top-[calc(50%-40px)] left-[calc(50%-40px)] rounded-full flex justify-center items-center animate-[likeShowUp_0.5s_ease-out_forwards] z-20">
+          <Heart size={164} className="text-red500" isFill />
+        </div>
+      ) : undefined}
+      {pause ? (
+        <div className="absolute w-[80px] h-[80px] bg-[rgba(0,0,0,0.4)] top-[calc(50%-40px)] left-[calc(50%-40px)] rounded-full flex justify-center items-center animate-[showUp_0.5s_ease-out_forwards] z-20">
+          <Pause className="text-white" />
+        </div>
+      ) : undefined}
+      {!pause ? (
+        <div className="absolute w-[80px] h-[80px] bg-[rgba(0,0,0,0.4)] top-[calc(50%-40px)] left-[calc(50%-40px)] rounded-full flex justify-center items-center animate-[showUp_0.5s_ease-out_forwards] z-20">
+          <Play isFill className="text-white" />
+        </div>
+      ) : undefined}
+      {!commentOpen && !shareOpen && (
+        <>
+          <Content />
+          <SideBar setCommentOpen={handleCommentOpen} setLike={handleLike} like={like} setShareOpen={handleShareOpen} />
+        </>
+      )}
+      {commentOpen && <Comments isOpen={commentOpen} setIsClose={handleCommentOpen} />}
+      {shareOpen && <ShareContainer setIsClose={handleShareOpen} />}
+    </div>
+  )
+}
+
+export default VideoPlayer
