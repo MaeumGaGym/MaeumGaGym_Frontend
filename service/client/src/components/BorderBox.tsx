b328838fd1ec6d95f6285230394dbe1a40ed9609
ojinikim <ojinikim@gmail.com>
2024-05-08 23:35:19 +0900

    EDIT :: add onClick parameter in IconButton

--- service/client/src/components/BorderBox.tsx
+++ service/client/src/components/BorderBox.tsx
@@ -4,9 +4,10 @@ import React from 'react'
 interface PropsType {
   children?: React.ReactNode
   className?: string
+  onClick?: () => void
 }
 
-const BorderBox = ({ children, className }: PropsType) => {
+const BorderBox = ({ children, className, onClick }: PropsType) => {
   return <div className={`border border-gray100 rounded-2xl ${className}`}>{children}</div>
 }
 
--- service/client/src/components/IconButton.tsx
+++ service/client/src/components/IconButton.tsx
@@ -7,13 +7,14 @@ interface PropsType {
   alt?: string
   title: string
   text: string | [string, string]
+  onClick?: () => void
 }
 
-const IconButton = ({ src, icon, alt, title, text }: PropsType) => {
+const IconButton = ({ src, icon, alt, title, text, onClick }: PropsType) => {
   return (
     <BorderBox className="px-8 py-10 flex flex-col justify-between w-60 h-60 sm:w-20 flex-1 cursor-pointer break-keep">
       {icon || (src && <Image src={src} alt={alt || '이미지'} width={40} height={40} />)}
-      <div className="flex flex-col h-[72px] justify-between">
+      <div onClick={onClick} className="flex flex-col h-[72px] justify-between">
         <span className="text-black text-labelLarge sm:text-labelMedium">{title}</span>
         <p className="text-gray600 text-bodySmall sm:text-bodyTiny h-9">
           {typeof text === 'string' ? (
