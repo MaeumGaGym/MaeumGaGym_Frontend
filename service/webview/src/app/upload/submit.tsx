b889dee9f698ce32bc3c88c0b1efb70bc178d647
kimgyuha <kimgyuha25@gmail.com>
2024-05-09 23:19:44 +0900

    FEAT :: pickle redux 적용

--- service/webview/src/app/upload/submit.tsx
+++ service/webview/src/app/upload/submit.tsx
@@ -1,6 +1,11 @@
+'use client'
+import { RootState } from '@/utils/store/store'
 import { Button } from '@package/ui'
+import { useSelector } from 'react-redux'
 
 const Submit = () => {
+  const { title, content, file } = useSelector((state: RootState) => state.pickle)
+
   return (
     <div className="p-5 lg:w-[448px] md:w-[448px] sm:w-full fixed bottom-0">
       <Button className="w-full">피클 게시하기</Button>
--- service/webview/src/app/upload/tag.tsx
+++ service/webview/src/app/upload/tag.tsx
@@ -1,12 +1,22 @@
+'use client'
+import { deleteTagPickleUpload } from '@/utils/store/modules/pickleUpload'
+import { RootState } from '@/utils/store/store'
 import { Button, Close, Tag } from '@package/ui'
+import { useRouter } from 'next/navigation'
+import { useDispatch, useSelector } from 'react-redux'
 
 const Tags = () => {
+  const { tag } = useSelector((state: RootState) => state.pickle)
+  const nav = useRouter()
+
   return (
     <div className="px-5 py-3 gap-3 flex flex-wrap items-center">
-      <Button kind="silverDark" icon={<Tag />} fontSize="small">
+      <Button kind="silverDark" icon={<Tag />} fontSize="small" onClick={() => nav.push('/upload/search')}>
         태그 추가
       </Button>
-      <TagArticle title="태그1" />
+      {tag.map((v, idx) => (
+        <TagArticle title={v} key={v + idx} />
+      ))}
     </div>
   )
 }
@@ -16,11 +26,13 @@ interface TagArticleProps {
 }
 
 const TagArticle = ({ title }: TagArticleProps) => {
+  const dispatch = useDispatch()
+
   return (
-    <button className="h-min rounded-3xl bg-blue900 text-blue500 text-labelMedium flex gap-2 px-4 py-2 items-center justify-center">
+    <div className="h-min rounded-3xl bg-blue900 text-blue500 text-labelMedium flex gap-2 px-4 py-2 items-center justify-center">
       #{title}
-      <Close size={16} />
-    </button>
+      <Close size={16} onClick={() => dispatch(deleteTagPickleUpload({ data: title }))} />
+    </div>
   )
 }
 
