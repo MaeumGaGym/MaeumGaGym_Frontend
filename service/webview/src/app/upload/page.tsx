0a61272ca385ab2312a3408886c90087955223d8
kimgyuha <kimgyuha25@gmail.com>
2024-05-10 16:39:44 +0900

    FEAT :: pickle loading page
    loading 상태는 현재 상수로 관리. 연동 시 수정 필요

--- service/webview/src/app/upload/page.tsx
+++ service/webview/src/app/upload/page.tsx
@@ -3,15 +3,24 @@ import SelectMedia from './selectMidea'
 import ContentInput from './contentInput'
 import Tags from './tag'
 import Submit from './submit'
+import Loading from './Loading'
 
 const Upload = () => {
+  const loading = false
+
   return (
     <div className="bg-black w-full overflow-y-scroll flex flex-col gap-4 pb-[88px]">
-      <Title title="새로운 피클" type="goBack" />
-      <SelectMedia />
-      <ContentInput />
-      <Tags />
-      <Submit />
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <Title title="새로운 피클" type="goBack" />
+          <SelectMedia />
+          <ContentInput />
+          <Tags />
+        </>
+      )}
+      <Submit type={loading ? 'loading' : 'submit'} />
     </div>
   )
 }
--- service/webview/src/app/upload/submit.tsx
+++ service/webview/src/app/upload/submit.tsx
@@ -3,12 +3,15 @@ import { RootState } from '@/utils/store/store'
 import { Button } from '@package/ui'
 import { useSelector } from 'react-redux'
 
-const Submit = () => {
+interface SubmitProps {
+  type: 'loading' | 'submit'
+}
+const Submit = ({ type }: SubmitProps) => {
   const { title, content, file } = useSelector((state: RootState) => state.pickle)
 
   return (
     <div className="p-5 lg:w-[448px] md:w-[448px] sm:w-full fixed bottom-0">
-      <Button className="w-full">피클 게시하기</Button>
+      <Button className="w-full">{type === 'loading' ? '확인' : type === 'submit' && '피클 게시하기'}</Button>
     </div>
   )
 }
